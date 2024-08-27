8f9a131b4b431de54af3861328967f5958fd939f
Rafael Martinez <rafael.martinezcalvo@orange.fr>
2024-08-27 23:24:29 +0200

    Initial commit

---
+++ private_dot_config/zsh/dot_zsh_plugins.zsh
@@ -0,0 +1,6 @@
+if ! (( $+functions[zsh-defer] )); then
+  fpath+=( /home/rafael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-defer )
+  source /home/rafael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-defer/zsh-defer.plugin.zsh
+fi
+fpath+=( /home/rafael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-arzzen-SLASH-calc.plugin.zsh )
+zsh-defer source /home/rafael/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-arzzen-SLASH-calc.plugin.zsh/calc.plugin.zsh
